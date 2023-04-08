ba0cae4bc740ca43f27d6f270bcb0e83c9e1b287
Muxammadali <115696592+Mukhammaddaali@users.noreply.github.com>
2023-04-08 05:35:52 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -4,7 +4,7 @@ public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
 for (int i=0;i<height;i++){
     for (int j=0;j<length;j++){
-        if (i==0 || i==height-1 || j==0 || j==length) System.out.print(8); else System.out.print(" ");
+        if (i==0 || i==height-1 || j==0 || j==length-1) System.out.print(8); else System.out.print(" ");
     }
     System.out.println();
 }
