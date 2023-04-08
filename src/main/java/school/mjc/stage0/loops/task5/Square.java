c93b0ad3a1a132f0700c296b83918d4e068dd84c
Muxammadali <115696592+Mukhammaddaali@users.noreply.github.com>
2023-04-08 05:15:58 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -6,10 +6,8 @@ public class Square {
     public void printSquareFrom8s(int sideLength){
     for (int i=0;i<sideLength;i++){
         for (int j=0;j<sideLength;j++){
-           if (i==0 ||i==sideLength-1 ||j==0 ||j==sideLength-1)System.out.println(8);else System.out.println(" ");
-
+           if (i==0 || i==sideLength-1 || j==0 || j==sideLength) System.out.print(8);else System.out.println(" ");
         }
-        System.out.println();
     }
     }
     public void main(String []args){
